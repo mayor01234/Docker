d7acdd3c96fb11cf152753d8bcef074b7791e873
Olu Okeyoyin <vantage5624@gmail.com>
2023-04-29 23:52:28 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to Docker Class at JJTECH!"
+
+@app.route('/how are you')
+def hello():
+    return 'Yes Prof, I sure do. I love DevOps?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
